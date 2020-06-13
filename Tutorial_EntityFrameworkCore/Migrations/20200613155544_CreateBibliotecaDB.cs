453bd13720d788d4afeaaa0c4bd426e5b3ba5404
Estiven De Jesus Padilla Santos <estivenps26@gmail.com>
2020-06-13 11:57:44 -0400

    Creando la migracion

---
+++ Tutorial_EntityFrameworkCore/Migrations/20200613155544_CreateBibliotecaDB.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tutorial_EntityFrameworkCore.Migrations
+{
+    public partial class CreateBibliotecaDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libro",
+                columns: table => new
+                {
+                    LibroId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(nullable: true),
+                    Autor = table.Column<string>(nullable: true),
+                    Genero = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libro", x => x.LibroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestamo",
+                columns: table => new
+                {
+                    PrestamoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LibroId = table.Column<int>(nullable: false),
+                    ClienteId = table.Column<int>(nullable: false),
+                    FechaPrestamo = table.Column<DateTime>(nullable: false),
+                    FechaDevolucion = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamo", x => x.PrestamoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Libro");
+
+            migrationBuilder.DropTable(
+                name: "Prestamo");
+        }
+    }
+}
