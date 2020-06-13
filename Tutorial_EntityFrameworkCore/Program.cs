1c4515b5df3ee4736a9936876a2c26ff4ee50ae1
Estiven De Jesus Padilla Santos <estivenps26@gmail.com>
2020-06-13 11:32:32 -0400

    Add project files.

---
+++ Tutorial_EntityFrameworkCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial_EntityFrameworkCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
