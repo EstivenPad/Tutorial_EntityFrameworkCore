25931df31f9a81c9fb6b345f7376b1ad7b240593
Estiven De Jesus Padilla Santos <estivenps26@gmail.com>
2020-06-13 11:51:33 -0400

    Creando clase contexto

---
+++ Tutorial_EntityFrameworkCore/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tutorial_EntityFrameworkCore.Models;
+
+namespace Tutorial_EntityFrameworkCore
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Libro> Libro { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Prestamo> Prestamo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=BibliotecaDB;Trusted_Connection=True;" )
+        }
+    }
+}
